9b01a27d41fc695a271ee27b327a510aec497ec8
Toby Ho <airportyh@gmail.com>
2015-03-25 00:31:48 -0400

    Made use of commander.

--- cli.js
+++ cli.js
@@ -2,9 +2,28 @@
 
 var fs = require('fs')
 var typos = require('./typos')
-var cmdLn = require('cmd-ln')
+var program = require('commander')
 
-cmdLn(function(filename, typoType){
+program
+  .usage('[options] file')
+  .option('-t, --type [type]', 'Type of typo')
+  .on('--help', function(){
+    console.log('  Available types:\n')
+    console.log(Object.keys(typos).map(function(line){
+      return '    ' + line
+    }).join('\n'))
+  })
+
+program.parse(process.argv)
+
+var filename = program.args[0]
+if (!filename){
+  program.outputHelp()
+  process.exit()
+}
+main(filename, program.type || 'random')
+
+function main(filename, typoType){
   console.log('Reading', filename)
   var contents = fs.readFileSync(filename).toString()
   var typo = typos[typoType]
@@ -14,4 +33,4 @@ cmdLn(function(filename, typoType){
   }
   fs.writeFileSync(filename, typo(contents))
   console.log('Wrote', filename)
-})
+}
